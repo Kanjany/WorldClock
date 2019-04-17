f9ec7ff491e8ba9647a6ba9e4da573181e4b39be
Svavar16 <spg009@gmail.com>
2019-04-17 00:11:51 +0000

    added local time

--- WorldClockWebsite/main.js
+++ WorldClockWebsite/main.js
@@ -1,2 +1,22 @@
 var lat;
 var long;
+
+function MyLocTime() {
+    var time = new Date();
+    var timestring = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+    document.getElementById('World-Clock').innerHTML = timestring;
+    setTimeout(MyLocTime, 1000);
+}
+
+function NYTime() {
+    document.getElementById('World-Clock').innerHTML = "New York Time";
+}
+
+function TokyoTime() {
+    var time = new Date().toLocaleDateString("en-US", {timeZone: "Asia/Tokyo"});
+    document.getElementById('World-Clock').innerHTML = time;
+}
+
+function ReykjavikTime() {
+    document.getElementById('World-Clock').innerHTML = "Reykjavik Time";
+}
